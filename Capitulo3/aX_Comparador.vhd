a8280feca26209de9b89f865443dec44141cc6e6
jimdph <jimmydph999@gmail.com>
2023-07-24 14:58:36 -0500

    Solución 3.10
    Se empieza con la solución de l capítulo 3 con el ejercicio 3.10

---
+++ Capitulo3/aX_Comparador.vhd
@@ -0,0 +1,19 @@
+-- Ejercicio 3.10
+-- Comparador con salida 1 cuando X<Z
+
+library library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity aX_Comparador is
+    port (
+        x, z    : in std_logic_vector(2 downto 0);
+        f       : out std_logic;
+    );
+end entity aX_Comparador;
+
+architecture concurrente of aX_Comparador is
+    
+begin
+    f <= '1' when x < z else
+         '0'; 
+end architecture concurrente;
